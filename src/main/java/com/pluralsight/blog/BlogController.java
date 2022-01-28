34622f72d9a3d5562aed53fcc3bfb288f91eb9d1
Ivan Đapić <idjapic@gmail.com>
2022-01-28 22:22:47 +0100

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,4 +24,13 @@ public class BlogController {
         mm.put("posts",allPosts);
         return "home";
     }
+
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap){
+
+        modelMap.put("post", postRepository.findById(id));
+        return "post-details";
+    }
+
+
 }
